ae5f87b5f627b66fbd433c4639a6a975b695b806
dammycute <damilolaolawoore03@gmail.com>
2022-06-22 12:23:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// const dk = document.getElementById('dk')
+function sum(num1, num2, operator) {
+    if(operator === '+'){
+        console.log(num1 + num2)
+    }else if(operator === '-'){
+        console.log(num1 - num2)
+    }else if(operator === '/'){
+        console.log(num1 / num2)
+    }else if(operator === '*' || 'x'){
+        console.log(num1 * num2)
+    }
+}
+
+sum(3, 5, "/")
+
+
+
